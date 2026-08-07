0711b33d8a1f1c12f314e81cb197ccfdc8bc8655
kent <kent@github.com>
2026-08-07 14:39:40 +0800

    auto: sync 2026-08-07 14:39

---
+++ code/pointer-demo/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, K! This is a pointer-demo!\n");
+    //printf("sizeof(int) = %zu\n", sizeof(int));
+    //printf("sizeof(char) = %zu\n", sizeof(char));
+    return 0;
+}
