166e5363ab91f6105e069ad4fa7db4ff98eebb4b
kent <kent@github.com>
2026-08-07 13:52:31 +0800

    init: 建立学习目录结构

---
+++ code/hello/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, K! Day 1 of embedded transition\n");
+    printf("sizeof(int) = %zu\n", sizeof(int));
+    printf("sizeof(char) = %zu\n", sizeof(char));
+    return 0;
+}
